8f996697eee2ab2d98bc3265ac0e87e1b908b874
unknown <technic.cup@gmail.com>
2024-04-06 01:14:14 +0200

    Updated default project

--- modificator/framework/assets/defaultProject/src/layouts/root/Container.mjs
+++ modificator/framework/assets/defaultProject/src/layouts/root/Container.mjs
@@ -6,4 +6,4 @@ export const Container = new CjsComponent((data) => {
     `;
 });
 
-Container.importStyle('./src/layouts/main/styles/Container.css');
+Container.importStyle('./src/layouts/root/styles/Container.css');
