8f2cb695247eb5528dc431e6d13073f88a117cf9
Mikołaj Batorski <technic.cup@gmail.com>
2024-11-08 19:19:55 +0100

    Created base for download util

---
+++ cjs/utils/public/CjsDownloadUtil.js
@@ -0,0 +1,29 @@
+
+/**
+ * 
+ * @param {string} path 
+ * @param {string} filename 
+ */
+async function download(path, filename = null) {
+    try {
+        const response = await fetch(path);
+
+        if (!response.ok) {
+            return console.log(`${CJS_PRETTY_PREFIX_X}Couldn't download file ${response.statusText}`);
+        }
+
+        const blob = await response.blob();
+
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        URL.revokeObjectURL(link.href);
+    } catch (error) {
+        console.log(`${CJS_PRETTY_PREFIX_X}Error downloading file`, error);
+    }
+}
