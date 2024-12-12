e02e66837754f37a210c60903d3c150fe00db072
Mikołaj Batorski <technic.cup@gmail.com>
2024-12-12 14:27:55 +0100

    Added supportive methods

---
+++ cjs/utils/public/CjsStringUtil.js
@@ -0,0 +1,10 @@
+const CjsString = {
+    /**
+     * Remove Html tags from the input, keeping the inner Html content
+     * @param {string} input 
+     * @returns {string}
+     */
+    removeHtmlTags: function(input) {
+        return input.replace(/<[^>]*>/g, '');
+    }
+}
