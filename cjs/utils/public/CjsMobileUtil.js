66bdbb46cdbeb73c984a75e9838c69d21850602f
technic404 <technic.cup@gmail.com>
2025-04-05 09:08:00 +0200

    Added isIOS check on the user agent

--- cjs/utils/public/CjsMobileUtil.js
+++ cjs/utils/public/CjsMobileUtil.js
@@ -3,7 +3,14 @@ const CjsMobile = {
      * Performs basic check if user visited website on mobile device
      * @returns {boolean}
      */
-    isMobile: () => {
+    isMobile() {
         return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    },
+    /**
+     * Checks if the user is on an iOS device
+     * @returns {boolean}
+     */
+    isIOS() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
     }
 };
