1c27b9803bafe9fedeb99efc86b5bde12178cc80
technic404 <technic.cup@gmail.com>
2026-01-14 11:29:36 +0100

    Update default project Container template

--- cmd/framework/assets/default-project/src/layouts/root/Container.mjs
+++ cmd/framework/assets/default-project/src/layouts/root/Container.mjs
@@ -1,14 +1,15 @@
+export const Container = new class Container extends CjsComponent {
+    data = { };
 
-export const Container = new class Container extends CjsComponent { constructor() { super((data) => {
-    
-    return `
-        <div class="container"></div>
-    `;
-    });}
+    _() {
+        return `
+            <div class="container"></div>
+        `;
+    }
 
-    data = { }; 
-    
     /** Settings */
     _renderData = this.data;
     _cssStyle = './src/layouts/root/_styles/Container.css';
 }
+
+Container.fillHeight();
