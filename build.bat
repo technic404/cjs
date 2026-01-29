782326ed52f3ad3b2b19484226b2f2cdfd8a3530
technic404 <technic.cup@gmail.com>
2026-01-29 14:53:15 +0100

    Update build script

--- build.bat
+++ build.bat
@@ -4,35 +4,56 @@ setlocal
 :: Prompt for input
 set /p userInput="Enter version number: "
 
-:: Remove the build folder if it exists, then create a new one
+echo Recreating build folder
+
 if exist build (
     rd /s /q build
 )
 mkdir build
 
-:: Copy the ./cmd folder to ./build/cmd
+echo Copying ./cmd folder to ./build/cmd
+
 xcopy /e /i /y cmd build\cmd >nul
 
-:: Copy the ./common folder to ./build/common
+echo Copying ./common folder to ./build common
+
 xcopy /e /i /y common build\common >nul
 
-:: Remove the ./build/cmd/node_modules folder
+echo Removing ./build/cmd/node_modules folder
+
 if exist build\cmd\node_modules (
     rd /s /q build\cmd\node_modules
 )
 
-:: Remove the ./build/cmd/package-lock.json file
+echo Removing ./build/cmd/package-lock.json file
+
 if exist build\cmd\package-lock.json (
     del /q build\cmd\package-lock.json 
 )
 
-:: Copy c.js to ./build/c.js
+echo Copying c.js to ./build/c.js
+
 copy c.js build\c.js >nul
 
-:: Copy install.bat to ./build/install.bat
+echo Copying install.bat to ./build/install.bat
+
 copy install.bat build\install.bat >nul
 
-:: Create a zip archive with the contents of the build folder
+echo Creating zip archive with the contents of the build folder
+
 powershell Compress-Archive -Path build\* -DestinationPath "build\cjs-v%userInput%.zip" -Force
 
-echo Archive cjs-v%userInput%.zip has been created.
+echo Creating a zip archive with the contents of the cmd folder
+
+powershell Compress-Archive -Path build\cmd\* -DestinationPath "build\cjs-cmd-v%userInput%.zip" -Force
+
+echo Preapring source folder
+
+mkdir build\source
+
+move /y build\c.js build\source\c.js
+move /y build\install.bat build\source\install.bat
+move /y build\cmd build\source\cmd
+move /y build\common build\source\common
+
+echo Successfully finished
