0847af1f41476f9dbf72c7e42716a23d08e9b355
technic404 <technic.cup@gmail.com>
2026-02-14 15:39:01 +0100

    Update CjsForm, add option for checkbox to store array alike values

--- cjs/objects/CjsForm.js
+++ cjs/objects/CjsForm.js
@@ -19,9 +19,10 @@ class CjsForm {
 
     /**
      * Serializes data from form element
+     * @param {{ checkboxesReadType?: "array"|"single", includeNoNames?: boolean }} options
      * @returns {object}
      */
-    serialize() {
+    serialize(options = {}) {
         const selects = Array.from(this.#element.querySelectorAll("select"));
         const inputs = Array.from(this.#element.querySelectorAll("input"));
         const textareas = Array.from(this.#element.querySelectorAll("textarea"));
@@ -31,15 +32,38 @@ class CjsForm {
         for(let i = 0; i < elements.length; i++) {
             const element = elements[i];
             const name = element.getAttribute("name");
-            const value =
-                element.getAttribute("type") === "checkbox"
-                    ? element.checked
-                    : element.getAttribute("type") === "file"
-                        ? element.files
-                        : element.value;
+
+            if(!name && !options.includeNoNames) continue;
+
+            const type = element.getAttribute("type");
+            const value = type in this.#valueProcessRules 
+                ? this.#valueProcessRules[type](element) 
+                : this.#valueProcessRules['*'](element);
+
             const key = name || i;
-        
+
             data[key] = value;
+        }
+
+        if(options.checkboxesReadType && options.checkboxesReadType === "array") {
+            const checkboxes = elements.filter(e => e.getAttribute("type") === "checkbox");
+
+            for(const checkbox of checkboxes) {
+                if(!checkbox.hasAttribute("name")) {
+                    console.log(`${CJS_PRETTY_PREFIX_X}Checkbox doesn't have a name attribute, but it's required when options.checkboxesReadType === array`, checkbox);
+                    continue;
+                }
+
+                const name = checkbox.getAttribute("name");
+
+                if(!name) continue;
+
+                if(!(name in data) || !Array.isArray(data[name])) data[name] = [];
+
+                if(!checkbox.checked) continue;
+
+                data[name].push(checkbox.value);
+            }
         }
 
         return data;
