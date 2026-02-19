40b4790cbd08126872446002f20f0d939dc7ad8b
technic404 <technic.cup@gmail.com>
2026-02-19 15:25:13 +0100

    Add capitalization util

--- cjs/utils/public/CjsStringUtil.js
+++ cjs/utils/public/CjsStringUtil.js
@@ -6,5 +6,16 @@ const CjsString = {
      */
     removeHtmlTags: function(input) {
         return input.replace(/<[^>]*>/g, '');
+    },
+    /**
+     * Capitalizes first letter of the string
+     * @param {string} str 
+     * @param {boolean} isCapitalized 
+     * @returns {string}
+     */
+    capitalizeFirst(str, isCapitalized = false) {
+        if (str.length === 0) return str;
+        
+        return (isCapitalized ? str[0].toUpperCase() : str[0].toLowerCase()) + str.slice(1);
     }
 };
